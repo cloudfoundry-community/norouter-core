aa434b322295bda0fae4128d3eb5bbf791feea23
Mike Heath <elcapo@gmail.com>
2015-03-02 15:22:27 -0700

    Added additional tests.

--- src/test/java/cloudfoundry/norouter/nats/NatsRouteProviderTest.java
+++ src/test/java/cloudfoundry/norouter/nats/NatsRouteProviderTest.java
@@ -19,14 +19,19 @@ import cf.nats.CfNats;
 import cf.nats.DefaultCfNats;
 import cf.nats.message.RouterRegister;
 import cf.nats.message.RouterStart;
+import cf.nats.message.RouterUnregister;
 import cloudfoundry.norouter.routingtable.RouteRegistrar;
 import nats.client.MockNats;
+import nats.client.spring.NatsServerReadyApplicationEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.testng.annotations.Test;
 
 import java.net.InetSocketAddress;
 import java.time.Duration;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
@@ -123,5 +128,63 @@ public class NatsRouteProviderTest {
 
 		verify(routeRegistrar).registerRoute(uri1, InetSocketAddress.createUnresolved(host, port), null, null, null);
 		verify(routeRegistrar).registerRoute(uri2, InetSocketAddress.createUnresolved(host, port), null, null, null);
+	}
+
+	@Test
+	public void unregistersRouteWithRouterUnregisterMessage() {
+		final RouteRegistrar routeRegistrar = mock(RouteRegistrar.class);
+
+		final String host = "8.8.8.8";
+		final int port = 4321;
+		final String uri1 = "nolongerused.address.com";
+		final String uri2 = "some.other.address";
+
+		final MockNats mockNats = new MockNats();
+		final CfNats nats = new DefaultCfNats(mockNats);
+		try (final NatsRouteProvider natsRouteProvider = NatsRouteProvider.create()
+				.nats(nats)
+				.routeRegistrar(routeRegistrar)
+				.build()) {
+			natsRouteProvider.start();
+			nats.publish(new RouterUnregister(host, port, uri1, uri2));
+		}
+
+		verify(routeRegistrar).unregisterRoute(uri1, InetSocketAddress.createUnresolved(host, port));
+		verify(routeRegistrar).unregisterRoute(uri2, InetSocketAddress.createUnresolved(host, port));
+	}
+
+	@Test
+	public void contextRefreshCausesStart() {
+		final MockNats mockNats = new MockNats();
+		final CfNats nats = new DefaultCfNats(mockNats);
+		try (final NatsRouteProvider natsRouteProvider = NatsRouteProvider.create()
+				.nats(nats)
+				.routeRegistrar(mock(RouteRegistrar.class))
+				.build()) {
+			natsRouteProvider.onApplicationEvent(new ContextRefreshedEvent(mock(ApplicationContext.class)));
+			assertTrue(natsRouteProvider.isStarted());
+		}
+	}
+
+	@Test
+	public void onNatsServerConnectionPublishRouterStart() {
+		final AtomicInteger routerStartCounter = new AtomicInteger();
+		final MockNats mockNats = new MockNats();
+		mockNats.subscribe("router.start", message -> {
+			routerStartCounter.incrementAndGet();
+		});
+		final CfNats nats = new DefaultCfNats(mockNats);
+		try (final NatsRouteProvider natsRouteProvider = NatsRouteProvider.create()
+				.nats(nats)
+				.routeRegistrar(mock(RouteRegistrar.class))
+				.build()) {
+			// Router greet gets published at start time
+			natsRouteProvider.start();
+			assertEquals(routerStartCounter.get(), 1);
+
+			// Router greet gets published when NATS client reconnects
+			natsRouteProvider.onApplicationEvent(new NatsServerReadyApplicationEvent(mockNats));
+			assertEquals(routerStartCounter.get(), 2);
+		}
 	}
 }
